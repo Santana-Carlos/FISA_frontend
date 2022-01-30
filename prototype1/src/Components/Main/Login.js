6d178a50dc24fe669d676003ac6585153fd5affa
Diego Colmenares <diegocolmenares03@gmail.com>
2022-01-30 00:08:46 -0500

    minor changes v2.0.0

--- prototype1/src/Components/Main/Login.js
+++ prototype1/src/Components/Main/Login.js
@@ -136,7 +136,7 @@ const Login = () => {
           color: "#ffffff",
         }}
       >
-        {"FISA © 2021 v" + process.env.REACT_APP_VERSION}
+        {"FISA © 2022 v" + process.env.REACT_APP_VERSION}
       </div>
 
       <Dialog
